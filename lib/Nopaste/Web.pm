7a47df5f39aeb21e463d11f4472e9d5673deaab9
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-20 22:04:06 +0900

    db config.

--- lib/Nopaste/Web.pm
+++ lib/Nopaste/Web.pm
@@ -3,6 +3,8 @@ use Mojo::Base 'Mojolicious';
 
 sub startup {
   my $self = shift;
+  my $config = $self->plugin('Config', { file => 'nopaste.conf' });
+  $self->attr( db => sub { Nopaste::DB->new( $config->{db} ) } );
 
   $self->plugin('PODRenderer');
 
