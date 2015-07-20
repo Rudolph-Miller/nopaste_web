d8501dfc79741027060e1959cefa18f038be7c65
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-20 21:45:47 +0900

    validate input.

--- lib/Nopaste/Web.pm
+++ lib/Nopaste/Web.pm
@@ -9,6 +9,7 @@ sub startup {
   my $r = $self->routes;
 
   $r->get('/')->to('root#index');
+  $r->post('/')->to('root#post');
 }
 
 1;
--- lib/Nopaste/Web/Controller/Root.pm
+++ lib/Nopaste/Web/Controller/Root.pm
@@ -1,9 +1,38 @@
 package Nopaste::Web::Controller::Root;
 use Mojo::Base 'Mojolicious::Controller';
+use Formvalidator::Lite;
+use HTML::FillInForm::Lite;
 
 sub index {
-  my $self = shift;
-  $self->render();
+    my $self = shift;
+    $self->render();
 }
+
+sub post {
+    my $self = shift;
+    my $validator = FormValidator::Lite->new($self->req);
+
+    $validator->set_message(
+        'title.not_null' => 'Title is Empty.',
+        'body.not_null' => 'Body is Empty.'
+    );
+
+    my $res = $validator->check(
+        title => [qw/NOT_NULL/],
+        body => [qw/NOT_NULL/]
+    );
+
+    if ($validator->has_error) {
+        my @messages = $validator->get_error_messages;
+        $self->stash->{error_messages} = \@messages;
+
+        my $html = $self->render_to_string('root/index');
+        return $self->render(
+            text => HTML::FillInForm::Lite->fill(\$html, $self->req->params),
+            format => 'html'
+        )
+    }
+}
+
 
 1;
